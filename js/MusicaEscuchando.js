3135a5078ba7afd8e59d2a999311583241e42902
ezequielmendoza <ezequielmendoza99@gmail.com>
2023-11-15 01:33:58 -0300

    Subida

--- js/MusicaEscuchando.js
+++ js/MusicaEscuchando.js
@@ -18,8 +18,10 @@ function actualizarImagenReproduciendo() {
 function actualizarEstrellaAlbumSonando() {
     if (localCancionesFav) {
         let resultado = localCancionesFav.find(el => parseInt(el.id) == datos.id && el.idAlbum == datos.idAlbum);
-        if (resultado !== null && resultado) {
+        if (resultado && resultado !== null) {
             imgContainer[0].children[1].src = albumSonando[0].src.replace("estrella1", "estrellaAmarilla");
+        }else{
+            imgContainer[0].children[1].src = albumSonando[0].src.replace("estrellaAmarilla", "estrella1");
         }
     } else {
         imgContainer[0].children[1].src = albumSonando[0].src.replace("estrellaAmarilla", "estrella1");
@@ -27,20 +29,29 @@ function actualizarEstrellaAlbumSonando() {
 }
 
 function manejarClickEnEstrella() {
-    if (localCancionesFav) {
-        let resultado = localCancionesFav.find(el => parseInt(el.id) == datos.id && el.idAlbum == datos.idAlbum) || [];
+    let localCancionesFav = JSON.parse(localStorage.getItem("cancionesFavoritas")) || [];
+    let datos = JSON.parse(localStorage.getItem("reproduciendo"));
+    const resultado = localCancionesFav.find(el => parseInt(el.id) === datos.id && el.idAlbum === datos.idAlbum);
 
-        if (resultado !== null || resultado) {
-            const actualizacion = localCancionesFav.filter(el => parseInt(el.id) !== datos.id && el.idAlbum !== datos.idAlbum);
+    if (resultado) {
+        if (localCancionesFav.length > 1) {
+            const actualizacion = [...localCancionesFav.filter(el => (parseInt(el.id) !== datos.id || el.idAlbum !== datos.idAlbum))];
+
             localStorage.setItem("cancionesFavoritas", JSON.stringify(actualizacion));
             imgContainer[0].children[1].src = imgContainer[0].children[1].src.replace("estrellaAmarilla", "estrella1");
         } else {
-            localCancionesFav.push({ ...datos });
+            localCancionesFav.splice(localCancionesFav.indexOf(resultado), 1);
             localStorage.setItem("cancionesFavoritas", JSON.stringify(localCancionesFav));
-            imgContainer[0].children[1].src = imgContainer[0].children[1].src.replace("estrella1", "estrellaAmarilla");
+            imgContainer[0].children[1].src = imgContainer[0].children[1].src.replace("estrellaAmarilla", "estrella1");
         }
+    } else {
+        // La canción no está en la lista, agregarla y cambiar la estrella a estrellaAmarilla
+        localCancionesFav.push({ ...datos });
+        localStorage.setItem("cancionesFavoritas", JSON.stringify(localCancionesFav));
+        imgContainer[0].children[1].src = imgContainer[0].children[1].src.replace("estrella1", "estrellaAmarilla");
     }
 }
+
 
 document.addEventListener("DOMContentLoaded", () => {
     actualizarImagenReproduciendo();
