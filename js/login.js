67af2fc244ec179bd8329e1b997461ca7dfb95ae
woscoff <73312435+woscoff@users.noreply.github.com>
2023-11-14 22:59:54 -0300

    Login funciona correcto

--- js/login.js
+++ js/login.js
@@ -5,17 +5,31 @@ document.addEventListener('DOMContentLoaded', function() {
         event.preventDefault();
 
         const usuario = document.querySelector('input[name="usuario"]').value;
-        const contraseña = document.querySelector('input[name="contraseña"]').value;
+        const contraseñaIngresada = document.querySelector('input[name="contraseña"]').value;
 
         const usuariosRegistrados = JSON.parse(localStorage.getItem('usuarios')) || [];
 
-        const usuarioExistente = usuariosRegistrados.find(user => user.nombre === usuario && user.contraseña === contraseña);
+        const usuarioExistente = usuariosRegistrados.find(user => user.nombre === usuario);
 
         if (usuarioExistente) {
-            alert('Inicio de sesión exitoso');
-            window.location.href = './views/home.html'; 
+            const contraseñaOriginal = deshacerTransformacion(usuarioExistente.contraseña);
+
+            if (contraseñaIngresada === contraseñaOriginal) {
+                alert('Inicio de sesión exitoso');
+                window.location.href = './views/home.html'; 
+            } else {
+                alert('Credenciales incorrectas. Por favor, verifica tu usuario y contraseña.');
+            }
         } else {
             alert('Credenciales incorrectas. Por favor, verifica tu usuario y contraseña.');
         }
     });
+
+    function deshacerTransformacion(contraseñaTransformada) {
+        const mitad = Math.ceil(contraseñaTransformada.length / 2);
+        const primeraMitad = contraseñaTransformada.slice(mitad);
+        const segundaMitad = contraseñaTransformada.slice(0, mitad);
+
+        return primeraMitad + segundaMitad;
+    }
 });
